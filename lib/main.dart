9be91c01073d403c4f18e2028434b5dc3ffd4cf3
tarripewe01@gmail.com <tarripewe01@gmail.com>
2021-08-19 16:25:39 +0700

    Add Bonus Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'ui/pages/bonus_page.dart';
 import 'ui/pages/get_started_page.dart';
+import 'ui/pages/main_page.dart';
 import 'ui/pages/sign_up_page.dart';
 import 'ui/pages/splash_page.dart';
 
@@ -18,6 +20,8 @@ class MyApp extends StatelessWidget {
         '/': (context) => SplashPage(),
         '/get-started': (context) => GetStartedPage(),
         '/sign-up': (context) => SignUpPage(),
+        '/bonus': (context) => BonusPage(),
+        '/main': (context) => MainPage(),
       },
     );
   }
