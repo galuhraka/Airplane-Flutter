d84acf1c47bfbaa38d5d74e72a71d323da04b38c
tarripewe01@gmail.com <tarripewe01@gmail.com>
2021-08-23 11:07:42 +0700

    Konfigurasi Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:airplane/cubit/page_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'ui/pages/bonus_page.dart';
-import 'ui/pages/detail_page.dart';
 import 'ui/pages/get_started_page.dart';
 import 'ui/pages/main_page.dart';
 import 'ui/pages/sign_up_page.dart';
 import 'ui/pages/splash_page.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,7 +24,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => PageCubit(),),
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
